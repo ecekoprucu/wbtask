5bc9b216518248dded7c3cdfe65ed8df39ed1629
ecekoprucu <ecekoprucu@gmail.com>
2021-09-19 18:07:15 +0300

    state change function

--- src/App.js
+++ src/App.js
@@ -5,27 +5,24 @@ import './App.css';
 function App () {
     const [componentType, setComponentType] = useState("stay");
     const [focusedInput, setFocusedInput] = useState('stay');
+
+    function setStates (type) {
+        setComponentType(type);
+        setFocusedInput(type);
+    }
+
     return (
         <div className="App">
             <div className="typesContainer">
-                <div className="typeContainer" onClick={() => {
-                    setComponentType('stay');
-                    setFocusedInput('stay');
-                }}>
+                <div className="typeContainer" onClick={() => setStates('stay')}>
                     {focusedInput==='stay' ? <span className="dot"/> : null}
                     <p className={focusedInput==='stay' ? 'boldText' : ''}>Stay</p>
                 </div>
-                <div className="typeContainer" onClick={() => {
-                    setComponentType('experience');
-                    setFocusedInput('experience');
-                }}>
+                <div className="typeContainer" onClick={() => setStates('experience')}>
                     {focusedInput==='experience' ? <span className="dot"/> : null}
                     <p className={focusedInput==='experience' ? 'boldText' : ''}>Experiences</p>
                 </div>
-                <div className="typeContainer" onClick={() => {
-                    setComponentType('rental');
-                    setFocusedInput('rental');
-                }}>
+                <div className="typeContainer" onClick={() => setStates('rental')}>
                     {focusedInput==='rental' ? <span className="dot"/> : null}
                     <p className={focusedInput==='rental' ? 'boldText' : ''}>Rental Car</p>
                 </div>
